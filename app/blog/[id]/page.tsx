afa21c2f7ac79f3ae0e4de4ee9b2bde120e06a1f
Linashem <lina.shemeneva@yandex.ru>
2023-12-05 16:29:50 +0300

    title

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+
 interface PostProps {
   params: {
     id: string;
   };
 }
+
+export function generateMetadata({params:{id}}:PostProps){
+return {
+  title:id
+}
+}
+
 
 const Post: React.FC<PostProps> = ({ params: { id } }) => {
   return (
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: "Blog",
+};
+
 
 const Page: React.FC = () => {
   return (
