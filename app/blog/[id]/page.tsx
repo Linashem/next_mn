bd596be08639652bca23ee159ee8e8b4f61f5036
Linashem <lina.shemeneva@yandex.ru>
2023-12-06 06:53:51 +0300

    получение постов, и 1 поста,  metadata- title [id]

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 
-
 interface PostProps {
   params: {
-    id: string;
+    id: number;
+    title: string;
+    body: string;
   };
 }
 
-export function generateMetadata({params:{id}}:PostProps){
-return {
-  title:id
-}
+async function getData(id: number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: { revalidate: 60 },
+    }
+  );
+  return response.json();
 }
 
 
-const Post: React.FC<PostProps> = ({ params: { id } }) => {
+export async function generateMetadata({ params: { id } }: PostProps) {
+  const post = await getData(id);
+
+  return {
+    title: post.title,
+  };
+}
+
+const Post: React.FC<PostProps> = async ({ params: { id } }) => {
+  const post = await getData(id);
   return (
     <>
-      <h1>Post {id}</h1>{" "}
+      {<h1 className="post_title">{post.title}</h1>}
+      <p>{post.body}</p>
     </>
   );
 };
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,37 @@
-import { Metadata } from 'next';
-import React from 'react';
+import { Metadata } from "next";
+import Link from "next/link";
+import React from "react";
+
+interface IPost {
+  id: number;
+  title: string;
+}
 
 export const metadata: Metadata = {
   title: "Blog",
 };
 
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: { revalidate: 60 },
+  });
+  return response.json();
+}
 
-const Page: React.FC = () => {
+const Blog: React.FC = async () => {
+  const posts = await getData();
   return (
     <>
-      Posts
+      <h2>Posts</h2>
+      <ul>
+        {posts.map((post: IPost) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title} </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
 
-export default Page;
+export default Blog;
